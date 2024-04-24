d2c2234849150d80b9ca7dabfd352179a392f5b5
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-24 22:27:06 +0300

    Try different ways of versioning- using request parameters, request headers and media type versioning.

--- src/main/java/com/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -17,4 +17,40 @@ public class VersioningPersonController {
         String lastName = "Doe";
         return new PersonV2(new Name(firstName, lastName));
     }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonUsingRequestParam() {
+        return new PersonV1("Eleri Doe");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonUsingRequestParam() {
+        String firstName = "John";
+        String lastName = "Doe";
+        return new PersonV2(new Name(firstName, lastName));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionUsingHeader() {
+        return new PersonV1("Eleri Doe");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionUsingHeader() {
+        String firstName = "John";
+        String lastName = "Doe";
+        return new PersonV2(new Name(firstName, lastName));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionUsingAcceptHeader() {
+        return new PersonV1("Eleri Doe");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionUsingAcceptHeader() {
+        String firstName = "John";
+        String lastName = "Doe";
+        return new PersonV2(new Name(firstName, lastName));
+    }
 }
