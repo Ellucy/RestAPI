8ca53e418798c4eaddc879aa7d8d0aa5c9d6b178
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-25 09:45:15 +0300

    Implement static filtering.

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
@@ -16,8 +17,10 @@ public class User {
     private Integer id;
 
     @Size(min=2, message="Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth date should be in the past")
+    @JsonProperty("birthdate")
     private LocalDate birthDate;
 }
